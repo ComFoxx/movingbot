f542bf938d690c35f24461df556e8e17814577ae
ComFoxx <contact@comfoxx.fr>
2020-07-07 12:25:39 +0200

    Mise à jour de Discord.js et commande status - Mise à jour de `Discord.js` vers la 12.2.0 - Ajout de la commande `status` pour changer le statut du robot - Suppression du fichier `package.lock.json` - Il peut désormais y avoir plusieurs administrateurs - Mise à jour du `README.md` - Ajout de `npm start`

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# MovingBot
+# MovingBot v1.1.0
 
-Ce robot lie deux comptes discord et les déplaces mutuellement lorsque l'un initie le déplacement.
+Ce robot lie deux comptes discord et les déplaces mutuellement des salons vocaux lorsque l'un initie le déplacement.
 
-L'administrateur peut :
+Les administrateurs peuvent :
  - envoyer une image en message privé au robot et il la définira en tant qu'avatar
  - envoyer en message privé "rename " et il changera son pseudonyme par ce qui suis
  - envoyer en message privé "*identifiant utilisateur*=*identifiant salon vocal*" et il déplacera l'utilisateur vers le salon vocal
-
-## TODO
-
- - Mettre à jour Discord.js
- - Permettre à tous les utilisateurs liés de changer le status du bot
+ - envoyer en message privé "status " suivi de "PLAYING ", "WATCHING " ou "LISTENING " et il changera son statut par le texte qui suit avec le prefixe demandé (respectivement en français "Joue à", "Regarde" et "Écoute")
--- index.js
+++ index.js
@@ -6,96 +6,145 @@ const config = JSON.parse(fs.readFileSync(process.argv[2] == null ? 'config.json
 
 const token = config.token
 const userAssociation = config.userAssociation
-const admin = config.admin
+const admins = config.admin
+
+var log = (info, error = false) => error ? console.error(`[${Date.now()}] ${info}`) : console.log(`[${Date.now()}] ${info}`)
+var notAdmins = []
 
 client.on('ready', () => {
-	console.log(`[${Date.now()}] ${client.user.username} (${client.user.id}) connecté`)
+	log(`${client.user.username} (${client.user.id}) connecté`)
 })
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-	if (userAssociation.map(a => a[0]).includes(newMember.id) || userAssociation.map(a => a[1]).includes(newMember.id)) {
-		if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
+	if (oldMember.channelID !== newMember.channelID) {
+		if (userAssociation.map(a => a[0]).includes(newMember.id) || userAssociation.map(a => a[1]).includes(newMember.id)) {
 			const otherMember = newMember.guild.member(userAssociation.map(a => a[0]).includes(newMember.id) ? userAssociation[userAssociation.map(a => a[0]).indexOf(newMember.id)][1] : userAssociation[userAssociation.map(a => a[1]).indexOf(newMember.id)][0])
-			if (newMember.voiceChannelID !== otherMember.voiceChannelID && otherMember.voiceChannelID != null) {
-				console.log(`[${Date.now()}] Tentative de déplacement de ${otherMember} vers ${newMember.voiceChannel}…`)
-				otherMember.setVoiceChannel(newMember.voiceChannelID)
+			if (otherMember !== null && otherMember.voice.channelID !== null && newMember.channelID !== otherMember.voice.channelID && newMember.guild === otherMember.voice.guild) {
+				log(`Tentative de déplacement de ${otherMember} vers ${newMember.channel}…`)
+				otherMember.voice.setChannel(newMember.channelID, `Déplacement automatique`)
 					.then(() => {
-						console.log(`[${Date.now()}] Déplacement de ${otherMember} vers ${newMember.voiceChannel} réussi`)
+						log(`Déplacement de ${otherMember} vers ${newMember.channel} réussi`)
 					})
 					.catch(e => {
-						newMember.send(`Je n\'ai pas réussi à déplacer ${otherMember} :\n${e}`)
-							.catch(w => console.error(`[${Date.now()}] Le message de déplacement échoué ne peut être envoyé :\n${w}`))
-						console.error(`[${Date.now()}] Déplacement de ${otherMember} vers ${newMember.voiceChannel} échoué :\n${e}`)
+						if (newMember.member !== null) {
+							newMember.member.send(`Je n'ai pas réussi à déplacer ${otherMember} : \`\`\`${e}\`\`\``)
+								.catch(w => log(`Le message de déplacement automatique échoué n'a pas pu être envoyé :\n${w}`, true))
+						}
+						log(`Déplacement de ${otherMember} vers ${newMember.channel} échoué :\n${e}`, true)
 					})
 			}
 		}
 	}
 })
 
-client.on('message', message => {
-	if (message.channel.type == 'dm' && message.author.id == admin) {
-		if (message.attachments.size !== 0) {
-			if (['.jpg', '.jpeg', '.png', '.gif'].includes(path.extname(message.attachments.first().filename))) {
-				const imageURL = message.attachments.first().url
-				console.log(`[${Date.now()}] Tentative de changement d\'avatar pour ${imageURL}…`)
-				client.user.setAvatar(imageURL)
+client.on('message', async message => {
+	if (message.channel.type === 'dm') {
+		if (admins.includes(message.author.id)) {
+			if (message.attachments.size !== 0) {
+				if (['.jpg', '.jpeg', '.png', '.gif'].includes(path.extname(message.attachments.first().name).toLowerCase())) {
+					const imageURL = message.attachments.first().proxyURL
+					log(`Tentative de changement d'avatar pour ${imageURL}…`)
+					client.user.setAvatar(imageURL)
+						.then(() => {
+							message.channel.send(`J'ai changé mon avatar pour ${imageURL}`)
+								.catch(e => log(`Le message de changement d'avatar réussi n'a pas pu être envoyé :\n${e}`, true))
+							log(`Changement d'avatar pour ${imageURL} réussi`)
+						})
+						.catch(e => {
+							message.channel.send(`Je n'ai pas réussi à changer mon avatar pour ${imageURL} : \`\`\`${e}\`\`\``)
+								.catch(w => log(`Le message de changement d'avatar échoué n'a pas pu être envoyé :\n${w}`, true))
+							log(`Changement d'avatar pour ${imageURL} échoué :\n${e}`, true)
+						})
+				}
+			} else if (message.content.startsWith('rename ')) {
+				const newUsername = message.content.substring(7)
+				log(`Tentative de changement de pseudonyme pour ${newUsername}…`)
+				client.user.setUsername(newUsername)
 					.then(() => {
-						message.channel.send(`J\'ai changé mon avatar pour ${imageURL}`)
-							.catch(e => console.error(`[${Date.now()}] Le message de changement d'avatar réussi ne peut être envoyé :\n${e}`))
-							console.log(`[${Date.now()}] Changement d\'avatar pour ${imageURL} réussi`)
+						message.channel.send(`J'ai changé mon pseudonyme pour \`${newUsername}\`\n` +
+						'Attention, Discord n\'autorise que 2 modifications de pseudonyme par heure.')
+							.catch(e => log(`Le message de changement de pseudonyme réussi n'a pas pu être envoyé :\n${e}`, true))
+						log(`Changement de pseudonyme pour ${newUsername} réussi`)
 					})
 					.catch(e => {
-						message.channel.send(`Je n\'ai pas réussi à changer mon avatar pour ${imageURL} :\n${e}`)
-							.catch(w => console.error(`[${Date.now()}] Le message de changement d'avatar échoué ne peut être envoyé :\n${w}`))
-						console.error(`[${Date.now()}] Changement d\'avatar pour ${imageURL} échoué :\n${e}`)
+						message.channel.send(`Je n'ai pas réussi à changer mon pseudonyme pour \`${newUsername}\` : \`\`\`${e}\`\`\``)
+							.catch(e => log(`Le message de changement de pseudonyme échoué n'a pas pu être envoyé :\n${e}`, true))
+						log(`Changement de pseudonyme pour ${newUsername} échoué :\n${e}`, true)
 					})
+			} else if (message.content.startsWith('status ')) {
+				const status = message.content.substring(7).match(/^(PLAYING|WATCHING|LISTENING) (.+)$/im)
+				if (status !== null) {
+					log(`Tentative de changement de statut pour le statut "${status[2]}" de type ${status[1]}…`)
+					client.user.setActivity(status[2], { type: status[1] })
+						.then(() => {
+							message.channel.send(`J'ai changé mon statut pour \`${status[0]}\``)
+								.catch(e => log(`Le message de changement de statut réussi n'a pas pu être envoyé :\n${e}`, true))
+							log(`Changement de statut pour ${status[0]} réussi`)
+						})
+						.catch(e => {
+							message.channel.send(`Je n'ai pas réussi à changer mon statut pour \`${status[0]}\` : \`\`\`${e}\`\`\``)
+								.catch(e => log(`Le message de changement de statut échoué n'a pas pu être envoyé :\n${e}`, true))
+							log(`Changement de statut pour ${status[0]} échoué :\n${e}`, true)
+						})
+				} else {
+					message.channel.send(`Le statut doit débuter par PLAYING, WATCHING ou LISTENING suivi du texte du statut séparé par un espace`)
+						.catch(e => log(`Le message de changement de statut impossible n'a pas pu être envoyé :\n${e}`, true))
+					log(`Changement de statut impossible avec la commande "${message.content}"`)
+				}
+			} else if (message.content.includes('=')) {
+				try {
+					const params = message.content.split('=')
+					const channel = await client.channels.fetch(params[1]).catch(e => {
+						message.channel.send(`Je n'ai pas trouvé le salon vocal ayant pour identifiant ${params[1]} : \`\`\`${e}\`\`\``)
+							.catch(w => log(`Le message de salon vocal non trouvé n'a pas pu être envoyé :\n${w}`, true))
+						log(`Salon vocal ${params[1]} non trouvé`, true)
+						throw e
+					})
+					if (channel.type !== 'voice') {
+						message.channel.send(`Le salon ${channel} n'est pas un salon vocal`)
+							.catch(e => log(`Le message de salon non vocal n'a pas pu être envoyé :\n${e}`, true))
+						log(`Le salon ${channel} n'est pas un salon vocal`, true)
+						throw `${channel} is not a voice channel`
+					}
+					const member = await channel.guild.members.fetch(params[0]).catch(e => {
+						message.channel.send(`Je n'ai pas trouvé l'utilisateur <@${params[0]}> sur le serveur du salon ${channel} : \`\`\`${e}\`\`\``)
+							.catch(w => log(`Le message d'utilisateur non trouvé n'a pas pu être envoyé :\n${w}`, true))
+						log(`L'utilisateur <@${params[0]}> n'a pas été trouvé sur le serveur du salon ${channel}`, true)
+						throw e
+					})
+					log(`Tentative de déplacement de ${member} vers ${channel}…`)
+					member.voice.setChannel(channel, `Déplacement demandé par un administrateur`)
+						.then(() => {
+							message.channel.send(`J'ai déplacé ${member} vers ${channel}`)
+								.catch(e => log(`Le message de déplacement réussi n'a pas pu être envoyé :\n${e}`, true))
+							log(`Déplacement de ${member} vers ${channel} réussi`)
+						})
+						.catch(e => {
+							message.channel.send(`Je n'ai pas réussi à déplacé ${member} vers ${channel} : \`\`\`${e}\`\`\``)
+								.catch(e => log(`Le message de déplacement échoué n'a pas pu être envoyé :\n${e}`, true))
+							log(`Déplacement échoué :\n${e}`, true)
+						})
+				} catch (e) {
+					message.channel.send(`Déplacement impossible\nRappel : *IdentifiantUtilisateur*=*IdentifiantSalonVocal*`)
+						.catch(w => log(`Le message de déplacement impossible n'a pas pu être envoyé :\n${w}`, true))
+					log(`Déplacement impossible :\n${e}`, true)
+				}
+			} else {
+				message.channel.send(`Désolé, je ne sais pas ce que vous voulez`)
+					.catch(e => log(`Le message d'incompréhension n'a pas pu être envoyé :\n${e}`, true))
 			}
-		} else if (message.content.startsWith('rename ')) {
-			const newUsername = message.content.substring(7)
-			console.log(`[${Date.now()}] Tentative de changement de pseudonyme pour ${newUsername}…`)
-			client.user.setUsername(newUsername)
-				.then(() => {
-					message.channel.send(`J\'ai changé mon pseudonyme pour : ${newUsername}\n` +
-					'Attention, Discord n\'autorise que 2 modifications de pseudonyme par heure.')
-						.catch(e => console.error(`[${Date.now()}] Le message de changement de pseudonyme réussi ne peut être envoyé`))
-					console.log(`[${Date.now()}] Changement de pseudonyme pour ${newUsername} réussi`)
-				})
-				.catch(e => {
-					message.channel.send(`Je n\'ai pas réussi à changer mon pseudonyme pour : ${newUsername} :\n${e}`)
-					.catch(e => console.error(`[${Date.now()}] Le message de changement de pseudonyme échoué ne peut être envoyé`))
-					console.error(`[${Date.now()}] Changement de pseudonyme pour ${newUsername} échoué :\n${e}`)
-				})
-		} else if (message.content.includes('=')) {
-			try {
-				let params = message.content.split('=')
-				let channel = client.channels.get(params[1])
-				console.log(`[${Date.now()}] Tentative de déplacement de <@${params[0]}> vers ${channel.name}…`)
-				channel.guild.member(params[0]).setVoiceChannel(channel)
-					.then(() => {
-						message.channel.send(`J'ai déplacé <@${params[0]}> vers ${channel.name}`)
-						.catch(e => console.error(`[${Date.now()}] Le message de déplacement réussi ne peut être envoyé`))
-						console.log(`[${Date.now()}] Déplacement de ${params[0]} vers ${channel.name} réussi`)
-					})
-					.catch(e => {
-						message.channel.send(`Je n\'ai pas réussi à changer mon pseudonyme pour : ${newUsername} :\n${e}`)
-						.catch(e => console.error(`[${Date.now()}] Le message de déplacement échoué ne peut être envoyé`))
-						console.error(`[${Date.now()}] Déplacement échoué :\n${e}`)
-					})
-			} catch (e) {
-				message.channel.send(`Je n\'ai pas réussi à déplacer :\n${e}`)
-				.catch(e => console.error(`[${Date.now()}] Le message de déplacement échoué ne peut être envoyé`))
-				console.error(`[${Date.now()}] Déplacement échoué :\n${e}\nRappel : *IdentifiantUtilisateur*=*IdentifiantSalonVocal*`)
+		} else if (message.author.id !== client.user.id) {
+			if (!notAdmins.includes(message.author.id)) {
+				message.channel.send(`Je ne peux pas répondre à votre demande car vous n'êtes pas administrateur.`)
+					.catch(e => log(`Le message au non-administrateur n'a pas pu être envoyé :\n${e}`, true))
+				notAdmins.push(message.author.id)
 			}
 		}
 	}
 })
 
 client.on('error', e => {
-	console.error(`[${Date.now()}] ${e.name}: ${e.message}`)
-})
-
-client.on('disconnect', () => {
-	client.login(token)
+	log(`${e.name}: ${e.message}`, true)
 })
 
 client.login(token)
